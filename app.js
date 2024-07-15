ebbdde1ef4b61e04697cff9e194c7ca0180fa86f
sbsdigitalinfotech1 <151001895+sbsdigitalinfotech1@users.noreply.github.com>
2024-07-15 15:44:53 +0530

    Update app.js

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const express = require("express");
 const path = require("path");
 const bodyParser = require("body-parser");
 const app = express();
-const PORT = 8000;
+const PORT = process.env.PORT || 3001;
 
 // Define the allowed origin
 const allowedOrigin = "http://localhost:3000";
